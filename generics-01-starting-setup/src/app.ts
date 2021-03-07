41f86847de00822c20e0ca342b071c7b1dde0bc4
galen <galentaylor@gmail.com>
2021-03-06 18:25:45 -0800

    playing with promise errors

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -2,29 +2,52 @@
 
 const myPromise = new Promise((resolve, error ) => {
     if (true){
-        error('Some error happened')
+        error((reason: string) => console.log('Some error happened ' + reason))
     }
+
     setTimeout(() => {
         resolve('foo');
     }, 300);
 });
 myPromise
     .then(value => {
+        throw 'some'
         return value + ' and bar';
-    }, () => { console.log("in then")})
+    })
     .then(value => {
         return value + ' and bar again';
     })
-    .then(value => {
-        return value + ' and again';
-    })
-    .then(value => {
-        return value + ' and again';
-    })
-    .then(value => {
-        console.log(value)
-    })
     .catch(err => {
-        console.log('In catch')
-        console.log(err)
+        console.log('In catch ')
+        err(' WoooWeee')
     })
+
+// myPromise
+//     .then(value => {
+//         return value + ' and bar';
+//     }, (reason) => {
+//         console.log("in then "+ reason);
+//         reason("wow crazy")
+//     })
+//     .then(value => {
+//         return value + ' and bar again';
+//     }, (reason) => {
+//         console.log("in then 1 " + reason)
+//     })
+//     .then(value => {
+//         return value + ' and again';
+//     }, (reason) => {
+//         console.log("in then" + reason)
+//     })
+//     .then(value => {
+//         return value + ' and again';
+//     }, (reason) => {
+//         console.log("in then" + reason)
+//     })
+//     .then(value => {
+//         console.log(value)
+//     })
+//     .catch(err => {
+//         console.log('In catch')
+//         console.log(err)
+//     })
