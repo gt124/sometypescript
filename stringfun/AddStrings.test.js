9ca18966925fc5e9dd22718413ed7438ad74a3f3
galen <galentaylor@gmail.com>
2021-03-09 20:09:55 -0800

    stringfun

--- stringfun/AddStrings.test.js
+++ stringfun/AddStrings.test.js
@@ -1,11 +1,13 @@
 //import {AddStrings} from './AddStrings'; //Can't get imports to work
 const {AddStrings} = require("./AddStrings")
 
-test('PackStrings test', () => {
+test('largeLengthOnTop test', () => {
     let sut = new AddStrings("2", "1");
+    let output = sut.largeLengthOnTop()
+    console.log(output)
+
 //    expect(sut.CalcArea(1, 3)).toBe(3);
 //    let output = sut.PackStrings();
-//    console.log(output)
 
     // joe = {
     //     asut: "some",
@@ -13,3 +15,10 @@ test('PackStrings test', () => {
     // }
     console.log("test")
 });
+test('loopThroughAndAdd test', () => {
+
+    let sut = new AddStrings("2", "1");
+    let output = sut.loopThroughAndAdd("12345", "678")
+    console.log(output)
+})
+;
--- stringfun/AddStrings.ts
+++ stringfun/AddStrings.ts
@@ -5,21 +5,52 @@ export class AddStrings{
     }
 
     AddStrings(){
-        //pack the strings so they are the same size
+        //Get the largest length one on top.
+
+       // const orderedStrings = this.largeLengthOnTop();
+
 
         //loop through strings adding with a carry
 
         //return result
     }
 
-    PackStrings(){
+    addTwoNumbersAndCarry(_firstNumber: string, _secondNumber: string, _carry: string = "0"){
+        //Take in letters, convert to numbers and add, then if it's a carry
+    }
+
+    loopThroughAndAdd( outFirst: string, outSecond: string){
+        const largeCount = outFirst.length;
+        const smallCount = outSecond.length;
+
+        for(let i = largeCount - 1; i >= 0; i--)
+        {
+            console.log(outFirst[i])
+            //Add two numbers and carry
+        }
+        return {
+            outFirst: largeCount,
+            outSecond: smallCount
+        }
+    }
+
+    largeLengthOnTop(){
         if(this.first.length === this.second.length ){
             return {
                 outFirst: this.first,
                 outSecond: this.second
             }
         }
-        return null
+        if(this.first.length > this.second.length) {
+            return {
+                outFirst: this.first,
+                outSecond: this.second
+            }
+        }
+        return {
+            outFirst: this.second,
+            outSecond: this.first
+        }
     }
 }
 
